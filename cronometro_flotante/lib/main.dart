595a590d812630131e107f89dbb9a716de69f1f3
kendallvargasdg-design <kendall.vargas.dg@gmail.com>
2026-05-08 12:51:48 -0500

    Remove overlayMain function and import
    Removed unused overlayMain function and related import.

--- cronometro_flotante/lib/main.dart
+++ cronometro_flotante/lib/main.dart
@@ -1,37 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'main_screen.dart';
-import 'overlay_widget.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const CronometroApp());
 }
 
-@pragma("vm:entry-point")
-void overlayMain() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      backgroundColor: Colors.red,
-      body: Center(
-        child: Text(
-          'OVERLAY\nACTIVO',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    ),
-  ));
-}
-
 class CronometroApp extends StatelessWidget {
   const CronometroApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
